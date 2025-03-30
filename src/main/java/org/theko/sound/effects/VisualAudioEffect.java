bf3c771d34ef1115ec131b2604f1691969d570ec
theko <krasnobaev.sasha11@gmail.com>
2025-03-30 23:48:13 +0300

    Change frame to panel

--- src/main/java/org/theko/sound/effects/VisualAudioEffect.java
+++ src/main/java/org/theko/sound/effects/VisualAudioEffect.java
@@ -1,26 +1,22 @@
 package org.theko.sound.effects;
 
-import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.Timer;
 
 import org.theko.sound.AudioEffect;
 import org.theko.sound.AudioFormat;
 
 public abstract class VisualAudioEffect extends AudioEffect {
-    protected JFrame frame;
     protected final Timer repaintTimer;
 
     public VisualAudioEffect(Type type, AudioFormat audioFormat) {
         super(type, audioFormat);
-        initializeFrame();
+        initializePanel();
         repaintTimer = new Timer(1000 / 60, e -> repaint());
         repaintTimer.start();
     }
 
-    public abstract void initializeFrame();
-    public abstract void repaint();
-
-    public JFrame getFrame() {
-        return frame;
-    }
+    public abstract void initializePanel();
+    public abstract JPanel getPanel();
+    protected abstract void repaint();
 }
