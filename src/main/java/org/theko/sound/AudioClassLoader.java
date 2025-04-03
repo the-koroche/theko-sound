7a81d15fa047763326e2a6915a7bf9b7d5a4f323
theko <krasnobaev.sasha11@gmail.com>
2025-04-03 23:22:57 +0300

    Create class loader to load audio device realizations, and codecs

---
+++ src/main/java/org/theko/sound/AudioClassLoader.java
@@ -0,0 +1,31 @@
+package org.theko.sound;
+
+import org.reflections.Reflections;
+import org.reflections.ReflectionsException;
+import org.reflections.scanners.Scanners;
+import org.reflections.util.ConfigurationBuilder;
+
+public class AudioClassLoader {
+    private static final Reflections reflections = createReflections();
+
+    private static Reflections createReflections() {
+        Reflections reflections = null;
+        try {
+            reflections = new Reflections(new ConfigurationBuilder()
+                    .forPackages("") // Scan all packages.
+                    .addScanners(Scanners.SubTypes) // Look for subtypes.
+            );
+        } catch (ReflectionsException ex) {
+            ex.printStackTrace();
+            reflections = new Reflections(new ConfigurationBuilder()
+            .forPackages("org.theko.sound", "org.theko.sound.codec.formats") // Fallback: scan only predefined classes.
+                .addScanners(Scanners.SubTypes)
+            );
+        }
+        return reflections;
+    }
+
+    public static Reflections getReflections() {
+        return reflections;
+    }
+}
