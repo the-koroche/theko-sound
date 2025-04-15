d6ee64342f4fc08e5a2717a195db464733cd50ff
theko <krasnobaev.sasha11@gmail.com>
2025-04-16 01:02:43 +0300

    Add reverse effect

--- src/main/java/org/theko/sound/effects/ReverseEffect.java
+++ src/main/java/org/theko/sound/effects/ReverseEffect.java
@@ -1,5 +1,24 @@
 package org.theko.sound.effects;
 
-public class ReverseEffect extends {
-    
+import org.theko.sound.AudioEffect;
+import org.theko.sound.AudioFormat;
+
+public class ReverseEffect extends AudioEffect {
+    public ReverseEffect(AudioFormat audioFormat) {
+        super(Type.REALTIME, audioFormat);
+    }
+
+    @Override
+    protected float[][] process(float[][] samples) {
+        for (int ch = 0; ch < samples.length; ch++) {
+            float[] channel = samples[ch];
+            int len = channel.length;
+            for (int i = 0; i < len / 2; i++) {
+                float temp = channel[i];
+                channel[i] = channel[len - 1 - i];
+                channel[len - 1 - i] = temp;
+            }
+        }
+        return samples;
+    }
 }
