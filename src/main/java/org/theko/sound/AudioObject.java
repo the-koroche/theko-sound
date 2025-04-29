0e621a372c11ba7c6039084ec7d40de038f1fbb7
theko <krasnobaev.sasha11@gmail.com>
2025-04-30 00:02:38 +0300

    Remove "marker interface" comment

--- src/main/java/org/theko/sound/AudioObject.java
+++ src/main/java/org/theko/sound/AudioObject.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 public interface AudioObject extends Serializable {
     static final long serialVersionUID = 1L;
-    // Just a marker interface for audio objects, to use in the AudioPreset class.
 
     default String getName() {
         return this.getClass().getSimpleName();
