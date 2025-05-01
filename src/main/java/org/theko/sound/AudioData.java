8a8a5d9b084493e07445f0c1ad938329ba8ccfa2
the-koro <krasnobaev.sasha11@gmail.com>
2025-05-01 22:47:38 +0300

    Mark deprecated, and for removal

--- src/main/java/org/theko/sound/AudioData.java
+++ src/main/java/org/theko/sound/AudioData.java
@@ -1,5 +1,6 @@
 package org.theko.sound;
 
+@Deprecated (forRemoval = true)
 public class AudioData implements AudioObject {
     private final byte[] bytes;
     private final AudioFormat audioFormat;
