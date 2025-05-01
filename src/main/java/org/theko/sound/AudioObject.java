2c53d1278a98b0f5b1ca1748a4a3766c48353e08
the-koro <krasnobaev.sasha11@gmail.com>
2025-05-01 17:11:18 +0300

    Fix serialVersionUID

--- src/main/java/org/theko/sound/AudioObject.java
+++ src/main/java/org/theko/sound/AudioObject.java
@@ -3,7 +3,7 @@ package org.theko.sound;
 import java.io.Serializable;
 
 public interface AudioObject extends Serializable {
-    static final long serialVersionUID = 1L;
+    static final long serialVersionUID = -1;
 
     default String getName() {
         return this.getClass().getSimpleName();
