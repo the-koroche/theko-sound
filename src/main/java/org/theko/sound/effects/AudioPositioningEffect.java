f8fc06dd6e3a25054a953d6cb367e91c47efd3df
The-Koro <krasnobaev.sasha11@gmail.com>
2025-07-09 14:10:35 +0300

    Add AudioPositioningEffect class for real-time audio positioning with gain and pan controls

---
+++ src/main/java/org/theko/sound/effects/AudioPositioningEffect.java
@@ -0,0 +1,47 @@
+package org.theko.sound.effects;
+
+import java.util.List;
+
+import org.theko.sound.control.AudioControl;
+import org.theko.sound.control.FloatControl;
+import org.theko.sound.utility.SamplesUtilities;
+
+/**
+ * Represents an audio effect that allows for real-time audio positioning
+ * through gain and pan controls.
+ * 
+ * This effect can be applied to audio samples to adjust their perceived
+ * position in the stereo field.
+ * 
+ * @author Theko
+ * @since v2.1.0
+ */
+public class AudioPositioningEffect extends AudioEffect {
+
+    protected final FloatControl gainControl = new FloatControl("Gain", 0.0f, 2.0f, 1.0f);
+    protected final FloatControl panControl = new FloatControl("Pan", -1.0f, 1.0f, 0.0f);
+
+    protected final List<AudioControl> positioningControls = List.of(
+        gainControl,
+        panControl
+    );
+
+    public AudioPositioningEffect() {
+        super(Type.REALTIME);
+
+        allControls.addAll(positioningControls);
+    }
+
+    @Override
+    public void render(float[][] samples, int sampleRate, int length) {
+        SamplesUtilities.adjustGainAndPan(samples, gainControl.getValue(), panControl.getValue());
+    }
+
+    public FloatControl getGain() {
+        return gainControl;
+    }
+
+    public FloatControl getPan() {
+        return panControl;
+    }
+}
