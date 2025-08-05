83bb6e11f385248376dbf438486a2c2fb128bb64
The-Koro <krasnobaev.sasha11@gmail.com>
2025-08-05 12:51:06 +0300

    Update UnsupportedPortLinkException: change superclass from RuntimeException to AudioBackendException for clearer backend-related error hierarchy

--- src/main/java/org/theko/sound/backend/UnsupportedPortLinkException.java
+++ src/main/java/org/theko/sound/backend/UnsupportedPortLinkException.java
@@ -2,12 +2,12 @@ package org.theko.sound.backend;
 
 /**
  * This exception is thrown to indicate that an audio port link type is not supported by the audio backend.
- * It extends the {@link RuntimeException} class and provides constructors for various use cases.
+ * It extends the {@link AudioBackendException} class and provides constructors for various use cases.
  * 
  * @since v2.3.2
  * @author Theko
  */
-public class UnsupportedPortLinkException extends RuntimeException {
+public class UnsupportedPortLinkException extends AudioBackendException {
 
     public UnsupportedPortLinkException() {
         super();
