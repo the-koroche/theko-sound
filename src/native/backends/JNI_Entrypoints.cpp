da2abdca243af1392a9c5e7eaab6fc169701edf7
The-Koro <krasnobaev.sasha11@gmail.com>
2025-08-07 23:43:01 +0300

    Update JNI_Entrypoints.cpp: remove debug print statements

--- src/native/backends/JNI_Entrypoints.cpp
+++ src/native/backends/JNI_Entrypoints.cpp
@@ -8,7 +8,6 @@ extern "C" {
         JNIEnv *env = nullptr;
         vm->GetEnv((void**) &env, JNI_VERSION_1_6);
 
-        printf("JNI_OnLoad called. (If you see this, please remove printing)\n");
         getClassesCache(env); // initialize classes cache
 
         return JNI_VERSION_1_6;
@@ -17,8 +16,6 @@ extern "C" {
     JNIEXPORT void JNICALL JNI_OnUnload(JavaVM* vm, void* reserved) {
         JNIEnv *env = nullptr;
         vm->GetEnv((void**) &env, JNI_VERSION_1_6);
-
-        printf("JNI_OnUnload called. (If you see this, please remove printing)\n");
 
         getClassesCache(env)->release(env);
 
