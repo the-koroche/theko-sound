f5926bc5b778055b0c523ee6723737de54bbd7ed
The-Koro <krasnobaev.sasha11@gmail.com>
2025-08-19 22:49:02 +0300

    Update SoundPlayer: add license notice and isOpen method

--- src/main/java/org/theko/sound/SoundPlayer.java
+++ src/main/java/org/theko/sound/SoundPlayer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Alex Soloviov (aka Theko)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.theko.sound;
 
 import java.io.File;
@@ -19,7 +35,7 @@ public class SoundPlayer extends SoundSource {
     public SoundPlayer(AudioOutputLayer outputLine) {
         super();
         this.outputLine = outputLine;
-            this.outputLine.setRootNode(this);
+        this.outputLine.setRootNode(this);
     }
 
     public SoundPlayer() {
@@ -95,6 +111,10 @@ public class SoundPlayer extends SoundSource {
     @Override
     public void open(String file) throws FileNotFoundException, AudioCodecNotFoundException {
         this.open(new File(file));
+    }
+
+    public boolean isOpen() {
+        return outputLine.isOpen();
     }
 
     @Override
