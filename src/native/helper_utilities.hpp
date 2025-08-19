32f1a063b380631d5195bd6a5788b04d8bf6dd2e
The-Koro <krasnobaev.sasha11@gmail.com>
2025-08-19 23:12:12 +0300

    Update helper_utilities.hpp: add license notice; add UTF-8 literal memalloc; add suffixes for UTF-16 memalloc methods for clarity; add function to get readable HRESULT type message from winhr_defs.hpp

--- src/native/helper_utilities.hpp
+++ src/native/helper_utilities.hpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Alex Soloviov (aka Theko)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include <string.h>
@@ -10,6 +26,7 @@
 
 #ifdef _WIN32
 #include <combaseapi.h> // CoTaskMemAlloc / CoTaskMemFree
+#include "winhr_defs.hpp"
 #endif
 
 static char* format(const char* fmt, ...) {
@@ -46,6 +63,14 @@ static char* formatv(const char* fmt, va_list args) {
     return buf; // caller must free using 'free'
 }
 
+static char* memalloc_literal_utf8(const char* str) {
+    if (!str) return NULL;
+    size_t len = strlen(str) + 1;
+    char* result = (char*)malloc(len);
+    strcpy_s(result, len, str);
+    return result; // caller must free using 'free'
+}
+
 #ifdef _WIN32
 static wchar_t* utf8_to_utf16(const char* utf8) {
     if (!utf8) return NULL;
@@ -72,9 +97,16 @@ static char* utf16_to_utf8(const wchar_t* utf16) {
     WideCharToMultiByte(CP_UTF8, 0, utf16, -1, utf8, len, NULL, NULL);
     return utf8; // caller must free using 'free'
 }
-#endif
 
-static wchar_t* memalloc_literal(const wchar_t* str) {
+static char* com_memalloc_literal_utf8(const char* str) {
+    if (!str) return NULL;
+    size_t len = strlen(str) + 1;
+    char* result = (char*)CoTaskMemAlloc(len);
+    strcpy_s(result, len, str);
+    return result; // caller must free using 'CoTaskMemFree'
+}
+
+static wchar_t* memalloc_literal_utf16(const wchar_t* str) {
     if (!str) return NULL;
     size_t len = wcslen(str) + 1;
     wchar_t* result = (wchar_t*)malloc(len * sizeof(wchar_t));
@@ -82,12 +114,17 @@ static wchar_t* memalloc_literal(const wchar_t* str) {
     return result; // caller must free using 'free'
 }
 
-#ifdef _WIN32
-static wchar_t* com_memalloc_literal(const wchar_t* str) {
+static wchar_t* com_memalloc_literal_utf16(const wchar_t* str) {
     if (!str) return NULL;
     size_t len = wcslen(str) + 1;
     wchar_t* result = (wchar_t*)CoTaskMemAlloc(len * sizeof(wchar_t));
     wcscpy_s(result, len, str);
     return result; // caller must free using 'CoTaskMemFree'
+}
+
+static char* format_hr_msg(HRESULT hr) {
+    const char* msgBuf = get_hr_name(hr);
+    char* result = format("%s (HRESULT: 0x%08X)", msgBuf, hr);
+    return result; // caller must free using 'free'
 }
 #endif
