a7164d926b0c54781946b235fd5a5227c588c39c
The-Koro <krasnobaev.sasha11@gmail.com>
2025-08-20 11:20:59 +0300

    Update WASAPISharedOutput: add license notice; make class sealed and permit WASAPIExclusiveOutput

--- src/main/java/org/theko/sound/backend/wasapi/WASAPISharedOutput.java
+++ src/main/java/org/theko/sound/backend/wasapi/WASAPISharedOutput.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Alex Soloviov (aka Theko)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.theko.sound.backend.wasapi;
 
 import org.slf4j.Logger;
@@ -10,7 +26,7 @@ import org.theko.sound.backend.AudioBackendException;
 import org.theko.sound.backend.AudioOutputBackend;
 import org.theko.sound.backend.BackendNotOpenException;
 
-public class WASAPISharedOutput extends WASAPISharedBackend implements AudioOutputBackend {
+public sealed class WASAPISharedOutput extends WASAPISharedBackend implements AudioOutputBackend permits WASAPIExclusiveOutput {
 
     private static final Logger logger = LoggerFactory.getLogger(WASAPISharedOutput.class);
 
