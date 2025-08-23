3b0f4ca89d47c4ebb391aa2fd49f3e714880c217
The-Koro <krasnobaev.sasha11@gmail.com>
2025-08-23 13:47:55 +0300

    Update logger.hpp, logger_manager.hpp: add license notice; improve performance with logger classes cache; implement trace logging method

--- src/native/logger.hpp
+++ src/native/logger.hpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Alex Soloviov (aka Theko)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include <jni.h>
@@ -8,13 +24,60 @@
 
 #include "helper_utilities.hpp"
 
+class LoggerCache {
+public:
+    jclass factoryClass;
+    jclass loggerClass;
+    jmethodID getLogger;
+    jmethodID traceMethod;
+    jmethodID debugMethod;
+    jmethodID infoMethod;
+    jmethodID warnMethod;
+    jmethodID errorMethod;
+
+    LoggerCache(JNIEnv* env) {
+        factoryClass = env->FindClass("org/slf4j/LoggerFactory");
+        loggerClass = env->FindClass("org/slf4j/Logger");
+
+        getLogger = env->GetStaticMethodID(factoryClass, "getLogger", "(Ljava/lang/String;)Lorg/slf4j/Logger;");
+        traceMethod = env->GetMethodID(loggerClass, "trace", "(Ljava/lang/String;)V");
+        debugMethod = env->GetMethodID(loggerClass, "debug", "(Ljava/lang/String;)V");
+        infoMethod  = env->GetMethodID(loggerClass, "info",  "(Ljava/lang/String;)V");
+        warnMethod  = env->GetMethodID(loggerClass, "warn",  "(Ljava/lang/String;)V");
+        errorMethod = env->GetMethodID(loggerClass, "error", "(Ljava/lang/String;)V");
+
+        if (!isValid()) {
+            env->ThrowNew(env->FindClass("java/lang/RuntimeException"), "Logger failed to initialize");
+        }
+
+        factoryClass = (jclass) env->NewGlobalRef(factoryClass);
+        loggerClass = (jclass) env->NewGlobalRef(loggerClass);
+    }
+
+    bool isValid() {
+        return factoryClass && loggerClass && getLogger && traceMethod && debugMethod && infoMethod && warnMethod && errorMethod;
+    }
+
+    void release(JNIEnv* env) {
+        if (factoryClass) {
+            env->DeleteGlobalRef(factoryClass);
+            factoryClass = nullptr;
+        }
+        if (loggerClass) {
+            env->DeleteGlobalRef(loggerClass);
+            loggerClass = nullptr;
+        }
+    }
+};
+
+static LoggerCache* getLoggerCache(JNIEnv* env) {
+    static LoggerCache* loggerCache = new LoggerCache(env);
+    return loggerCache;
+}
+
 class Logger {
 private:
     jobject logger = nullptr;
-    jmethodID debugMethod = nullptr;
-    jmethodID infoMethod = nullptr;
-    jmethodID warnMethod = nullptr;
-    jmethodID errorMethod = nullptr;
 
     void log(JNIEnv* env, jmethodID method, const char* msg, va_list args) {
         if (!logger || !method || !msg) return;
@@ -33,69 +96,51 @@ private:
 
 public:
     Logger(JNIEnv* env, const char* name) {
-        jclass loggerFactory = env->FindClass("org/slf4j/LoggerFactory");
-        if (!loggerFactory) return;
-
-        jmethodID getLogger = env->GetStaticMethodID(
-            loggerFactory, "getLogger", "(Ljava/lang/String;)Lorg/slf4j/Logger;");
-        if (!getLogger) {
-            env->DeleteLocalRef(loggerFactory);
-            return;
-        }
-
-        jstring jname = env->NewStringUTF(name);
-        jobject loggerObj = env->CallStaticObjectMethod(loggerFactory, getLogger, jname);
+        LoggerCache* cache = getLoggerCache(env);
+        jstring jname = env->NewStringUTF(name);    
+        logger = env->CallStaticObjectMethod(cache->factoryClass, cache->getLogger, jname);
         if (env->ExceptionCheck()) {
             env->ExceptionDescribe(); 
             env->ExceptionClear();
         }
+
+        logger = (jobject) env->NewGlobalRef(logger);
+
         env->DeleteLocalRef(jname);
-        env->DeleteLocalRef(loggerFactory);
+    }
 
-        if (!loggerObj) return;
-        jclass loggerClass = env->GetObjectClass(loggerObj);
-
-        debugMethod = env->GetMethodID(loggerClass, "debug", "(Ljava/lang/String;)V");
-        infoMethod  = env->GetMethodID(loggerClass, "info",  "(Ljava/lang/String;)V");
-        warnMethod  = env->GetMethodID(loggerClass, "warn",  "(Ljava/lang/String;)V");
-        errorMethod = env->GetMethodID(loggerClass, "error", "(Ljava/lang/String;)V");
-
-        if (!debugMethod || !infoMethod || !warnMethod || !errorMethod) {
-            env->DeleteLocalRef(loggerClass);
-            env->DeleteLocalRef(loggerObj);
-            return;
-        }
-
-        logger = env->NewGlobalRef(loggerObj);
-        env->DeleteLocalRef(loggerClass);
-        env->DeleteLocalRef(loggerObj);
+    void trace(JNIEnv* env, const char* msg, ...) {
+        va_list args;
+        va_start(args, msg);
+        log(env, getLoggerCache(env)->traceMethod, msg, args);
+        va_end(args);
     }
 
     void debug(JNIEnv* env, const char* msg, ...) {
         va_list args;
         va_start(args, msg);
-        log(env, debugMethod, msg, args);
+        log(env, getLoggerCache(env)->debugMethod, msg, args);
         va_end(args);
     }
 
     void info(JNIEnv* env, const char* msg, ...) {
         va_list args;
         va_start(args, msg);
-        log(env, infoMethod, msg, args);
+        log(env, getLoggerCache(env)->infoMethod, msg, args);
         va_end(args);
     }
 
     void warn(JNIEnv* env, const char* msg, ...) {
         va_list args;
         va_start(args, msg);
-        log(env, warnMethod, msg, args);
+        log(env, getLoggerCache(env)->warnMethod, msg, args);
         va_end(args);
     }
 
     void error(JNIEnv* env, const char* msg, ...) {
         va_list args;
         va_start(args, msg);
-        log(env, errorMethod, msg, args);
+        log(env, getLoggerCache(env)->errorMethod, msg, args);
         va_end(args);
     }
 
