0a9dfdc27fc7cebd9652002c2ebc77f7459e05b2
The-Koro <krasnobaev.sasha11@gmail.com>
2025-08-24 21:30:30 +0300

    Update WaveformGenerator: include license notice; clean up Javadoc since tags; document generate method

--- src/main/java/org/theko/sound/WaveformGenerator.java
+++ src/main/java/org/theko/sound/WaveformGenerator.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2025 Alex Soloviov (aka Theko)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.theko.sound;
 
 /**
  * Utility class for generating waveforms.
  * 
- * @since v2.3.2
+ * @since 2.3.2
  * @author Theko
  */
 public class WaveformGenerator {
@@ -12,6 +28,13 @@ public class WaveformGenerator {
         throw new UnsupportedOperationException("This class cannot be instantiated.");
     }
 
+    /**
+     * Generates a waveform based on the specified type and value.
+     * 
+     * @param type The waveform type.
+     * @param value The value to generate the waveform for.
+     * @return The generated waveform value.
+     */
     public static float generate(WaveformType type, float value) {
         switch (type) {
             case SINE:
