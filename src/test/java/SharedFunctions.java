92e7ac0e9208c5ffb6ccc652839cc145b4d2c1be
The-Koro <krasnobaev.sasha11@gmail.com>
2025-08-26 21:12:04 +0300

    Update SharedFunctions: keep only chooseAudioFile method and remove others due to limited usage

--- src/test/java/SharedFunctions.java
+++ src/test/java/SharedFunctions.java
@@ -1,40 +1,12 @@
-import org.theko.sound.codec.AudioCodecException;
-import org.theko.sound.codec.AudioCodecs;
-import org.theko.sound.codec.AudioDecodeResult;
-
 import helpers.FileChooser;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public final class SharedFunctions {
 
-    public static AudioDecodeResult decodeAudioFile(String filePath) {
-        try {
-            return 
-                AudioCodecs.getCodec(
-                    AudioCodecs.fromExtension(
-                        getFileExtension(filePath)
-                    )
-                ).decode(new FileInputStream(filePath));
-        } catch (FileNotFoundException | AudioCodecException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-    }
-
     public static File chooseAudioFile() {
         FileChooser fc = new FileChooser();
         return fc.chooseFile(new FileNameExtensionFilter("Audio Files", "wav", "wave"));
-    }
-
-    public static String getFileExtension(String filepath) {
-        int index = filepath.lastIndexOf(".");
-        if (index > 0) {
-            return filepath.substring(index + 1);
-        }
-        return "";
     }
 }
