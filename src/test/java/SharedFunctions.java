a24a7670062bb0084b1321228f39b14af378b48f
The-Koro <krasnobaev.sasha11@gmail.com>
2025-08-26 21:09:46 +0300

    Update SharedFunctions: mark class as final; use FileChooser instead of AWT FileDialog

--- src/test/java/SharedFunctions.java
+++ src/test/java/SharedFunctions.java
@@ -1,43 +1,33 @@
-import java.awt.FileDialog;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
 import org.theko.sound.codec.AudioCodecException;
 import org.theko.sound.codec.AudioCodecs;
 import org.theko.sound.codec.AudioDecodeResult;
 
-import java.awt.Frame;
+import helpers.FileChooser;
 
-public class SharedFunctions {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public final class SharedFunctions {
+
     public static AudioDecodeResult decodeAudioFile(String filePath) {
         try {
-            return AudioCodecs.getCodec(AudioCodecs.fromExtension(getFileExtension(filePath))).decode(new FileInputStream(filePath));
+            return 
+                AudioCodecs.getCodec(
+                    AudioCodecs.fromExtension(
+                        getFileExtension(filePath)
+                    )
+                ).decode(new FileInputStream(filePath));
         } catch (FileNotFoundException | AudioCodecException ex) {
             ex.printStackTrace();
             return null;
         }
     }
 
-    public static String chooseAudioFile() {
-        FileDialog fileDialog = new FileDialog((Frame) null, "Select an audio file (*.wav, *.ogg, *.flac)", FileDialog.LOAD);
-        fileDialog.setFilenameFilter((dir, name) ->
-            name.endsWith(".wav") || name.endsWith(".ogg") || name.endsWith(".flac")
-        );
-
-        fileDialog.setVisible(true);
-
-        String directory = fileDialog.getDirectory();
-        String filename = fileDialog.getFile();
-
-        fileDialog.dispose();
-
-        if (directory != null && filename != null) {
-            return new File(directory, filename).getAbsolutePath();
-        } else {
-            System.out.println("No file selected.");
-            return null;
-        }
+    public static File chooseAudioFile() {
+        FileChooser fc = new FileChooser();
+        return fc.chooseFile(new FileNameExtensionFilter("Audio Files", "wav", "wave"));
     }
 
     public static String getFileExtension(String filepath) {
