897e5b9ae022de70f0ea48dfb766f98ae07468b5
The-Koro <krasnobaev.sasha11@gmail.com>
2025-11-08 16:55:08 +0200

    Fix SpectrogramVisualizer: correct amplitude coloring

--- src/main/java/org/theko/sound/visualizers/SpectrogramVisualizer.java
+++ src/main/java/org/theko/sound/visualizers/SpectrogramVisualizer.java
@@ -186,7 +186,7 @@ public class SpectrogramVisualizer extends AudioVisualizer {
 
             for (int y = 0; y < getHeight(); y++) {
                 float currentAmplitude = MathUtilities.clamp(interpolatedSpectrum[y], 0.0f, 1.0f);
-                int color = volumeColorProcessor.getColor(currentAmplitude);
+                int color = volumeColorProcessor.getColor(1f - currentAmplitude);
                 line.setRGB(0, getHeight() - y - 1, color);
             }
 
