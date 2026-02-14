03853f7e1f9f0ac1ed5cb941d06b73c2c08dd1a8
the-koroche <thekoroche@gmail.com>
2026-02-14 22:34:32 +0200

    Fix ThreadUtilities: correct virtual thread creation; set default name if null; add method to retrieve thread ID

--- src/main/java/org/theko/sound/utility/ThreadUtilities.java
+++ src/main/java/org/theko/sound/utility/ThreadUtilities.java
@@ -16,8 +16,6 @@
 
 package org.theko.sound.utility;
 
-import java.lang.reflect.Method;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.theko.sound.properties.AudioSystemProperties;
@@ -38,9 +36,21 @@ public final class ThreadUtilities {
     }
 
     /**
+     * Creates a new thread with default name.
+     * 
+     * @param name The name of the thread. If null, the default name will be used.
+     * @param type The type of thread to create.
+     * @param task The task to run in the thread.
+     * @return The created thread.
+     */
+    public static Thread startThread(ThreadType type, Runnable task) {
+        return startThread(null, type, Thread.NORM_PRIORITY, task);
+    }
+
+    /**
      * Creates a new thread.
      * 
-     * @param name The name of the thread.
+     * @param name The name of the thread. If null, the default name will be used.
      * @param type The type of thread to create.
      * @param task The task to run in the thread.
      * @return The created thread.
@@ -49,67 +59,108 @@ public final class ThreadUtilities {
         return startThread(name, type, Thread.NORM_PRIORITY, task);
     }
 
+
     /**
      * Creates a new thread.
      * 
-     * @param name The name of the thread.
+     * @param name The name of the thread. If null, the default name will be used.
      * @param type The type of thread to create.
      * @param priority The priority of the thread.
      * @param task The task to run in the thread.
      * @return The created thread.
+     * @see #startThread(String, ThreadType, int, boolean, Runnable)
      */
     public static Thread startThread(String name, ThreadType type, int priority, Runnable task) {
         return startThread(name, type, priority, true, task);
     }
 
+    
     /**
-     * Creates a new thread.
+     * Creates a new thread with the specified name, type, priority, daemon status, and task.
+     * <p>
+     * Virtual threads does not support daemon and priority, and they will be ignored.
+     * If the thread type is not supported, it will fallback to platform thread.
      * 
-     * @param name The name of the thread.
+     * @param name The name of the thread. If null, the default name will be used.
      * @param type The type of thread to create.
      * @param priority The priority of the thread.
-     * @param daemon True if the thread is a daemon, false otherwise.
+     * @param daemon Whether the thread should be a daemon thread.
      * @param task The task to run in the thread.
      * @return The created thread.
+     * @throws IllegalArgumentException if the thread type or task is null, or if the thread type is not supported.
      */
     public static Thread startThread(String name, ThreadType type, int priority, boolean daemon, Runnable task) {
         if (type == null || task == null) {
             throw new IllegalArgumentException("Thread type or task cannot be null.");
         }
-        
+        logger.trace("Creating thread: {} {}", name, FormatUtilities.formatThreadInfo(type, priority));
+        Thread thread = null;
+
         switch (type) {
-            case PLATFORM:
-                Thread thread = new Thread(task, name);
-                thread.setPriority(priority);
-                thread.setDaemon(daemon);
-                thread.start();
-                logger.debug("Created platform thread: {} {}", name, FormatUtilities.formatThreadInfo(type, priority));
-                return thread;
             case VIRTUAL:
                 if (AudioSystemProperties.IS_SUPPORTING_VIRTUAL_THREADS) {
                     try {
-                        Class<?> threadClass = Thread.class;
-                        Method ofVirtual = threadClass.getMethod("ofVirtual");
-                        Object threadBuilder = ofVirtual.invoke(null);
-
-                        Method nameMethod = threadBuilder.getClass().getMethod("name", String.class);
-                        nameMethod.invoke(threadBuilder, name);
-                        
-                        Method startMethod = threadBuilder.getClass().getMethod("start", Runnable.class);
-                        Thread virtualThread = (Thread) startMethod.invoke(threadBuilder, task);
-                        
-                        logger.debug("Created virtual thread: {} {}", name, FormatUtilities.formatThreadInfo(type, priority));
-                        return virtualThread;
+                        thread = (Thread) Thread.class
+                        .getMethod("startVirtualThread", Runnable.class)
+                        .invoke(null, task);
+                        if (name != null && !name.isEmpty()) {
+                            thread.setName(name);
+                        } else {
+                            thread.setName("VirtualThread-" + getThreadId(thread));
+                        }
                     } catch (Throwable ex) {
-                        logger.error("Error creating virtual thread", ex);
-                        return null;
+                        logger.error("Error creating virtual thread, fallback to platform thread", ex);
                     }
                 } else {
-                    logger.error("Virtual threads are not supported in Java {}", Runtime.version().feature());
-                    return null;
+                    logger.info("Virtual threads are not supported in Java {}. Using platform thread.", Runtime.version().feature());
                 }
+                if (thread == null) {
+                    type = ThreadType.PLATFORM;
+                    //$FALLTHROUGH$
+                }
+                break;
+
+            case PLATFORM:
+                thread = new Thread(task);
+                if (name != null && !name.isEmpty()) {
+                    thread.setName(name);
+                } else {
+                    thread.setName("PlatformThread-" + getThreadId(thread));
+                }
+                thread.setPriority(priority);
+                thread.setDaemon(daemon);
+                thread.start();
+                break;
+
             default:
                 throw new IllegalArgumentException("Unsupported thread type: " + type);
+        }
+
+        logger.trace("Created thread: {} {}", thread.getName(), FormatUtilities.formatThreadInfo(type, priority));
+        return thread;
+    }
+
+    /**
+     * Returns the ID of the given thread.
+     * <p>
+     * If the thread is null, returns 0.
+     * <p>
+     * If the thread is not null, returns the result of calling
+     * {@link Thread#getId()} or {@link Thread#threadId()} depending on the
+     * Java version. If the reflection call fails, returns 0.
+     *
+     * @param thread the thread to get the ID of
+     * @return the ID of the thread, or 0 if the thread is null or the reflection call fails
+     */
+    public static long getThreadId(Thread thread) {
+        if (thread == null) {
+            return 0L;
+        }
+        String methodName = Runtime.version().feature() >= 19 ? "threadId" : "getId";
+        try {
+            return (long) Thread.class.getMethod(methodName).invoke(thread);
+        } catch (ReflectiveOperationException | SecurityException ex) {
+            return 0L;
         }
     }
 }
